e58828cf089168bcd39b2f0541d234c447e7691d
Samuel Adeniyi <sadeniyi@afexnigeria.com>
2023-06-23 13:22:35 +0100

    feat/updatedTodo

--- course-04/project/c4-final-project-starter-code/backend/src/businessLogic/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/businessLogic/todos.ts
@@ -1,7 +1,7 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS  from 'aws-sdk'
 import { CreateTodoPayload } from '../requests/CreateTodoRequest'
+import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 
@@ -29,5 +29,30 @@ export async function createTodo(newItem: CreateTodoPayload) {
   }).promise()
 
   return newItem
+}
+
+
+export async function updateTodo(todoId: any, userId: any, updatedItem: UpdateTodoRequest) {
+  
+  try {
+    await docClient.update({
+      TableName: todosTable,
+      Key: {
+        todoId,
+        userId
+      },
+      UpdateExpression: "set #name = :name, #dueDate = :dueDate, #done = :done",
+      ExpressionAttributeValues: {
+        ":name": updatedItem.name,
+        ":dueDate": updatedItem.dueDate,
+        ":done": updatedItem.done
+      },
+    }).promise()
+
+    return updatedItem
+  }
+  catch (err) {
+    console.error(`Updating Todo:${todoId}`, err);
+  }
 }
 
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -13,10 +13,19 @@ export const handler = middy(
     const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
     // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+    const updatedItem = updateTodo(todoId, getUserId(event), updatedTodo)
 
-
-    return undefined
-)
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        updatedItem
+      })
+    } 
+})
 
 handler
   .use(httpErrorHandler())
